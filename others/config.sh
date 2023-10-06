99004b3879c023227c6e5fec68dec46f5f1f5c0c
catx0rr <catx0rr@gmail.com>
2023-10-07 03:09:44 +0800

    added

---
+++ others/config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Raspberry pi - post installation config fix
+#
+# Add more post install config fix here:
+
+# Fix eth0 to managed mode
+
+function status() {
+    echo -e "$@"
+}
+
+function networkmanager_managed() {
+    rm -rf /etc/NetworkManager/NetworkManager.conf
+    wget https://raw.githubusercontent.com/offsecph/CREAMpi/master/others/NetworkManager.conf -P /etc/NetworkManager/
+    systemctl restart NetworkManager
+    nmcli con del 'Ifupdown (eth0)'
+    nmcli con mod 'Wired Connection' connection.id 'eth0'
+    nmcli con up 'Wired Connection'
+}
+
+function main() {
+    status '[*] Fixing NetworkManager to managed mode..'
+    networkmanager_managed
+    status '\n[+] Done.'
+}
+
+main
