96d2b4e20a284272ddf03c79f564752eb4008534
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-10-08 19:24:27 +0800

    Update enable_disable_bluetooth.sh

--- scripts/hardware/enable_disable_bluetooth.sh
+++ scripts/hardware/enable_disable_bluetooth.sh
@@ -5,18 +5,33 @@
 # Hardware locks persist only across reboots.
 #
 
-if [[ -z `cat /boot/config.txt | grep "dtoverlay=pi4-enable-bt"` ]]; then
-	echo "Bluetooth enabled.. Please reboot $HOSTNAME to apply changes"
-	echo -e "\n# Uncomment to enable bluetooth\ndtoverlay=pi4-enable-bt" >> /boot/config.txt
-	cat /boot/config.txt | grep 'dtoverlay=pi4-enable-bt' --color=none
-else
-	if [[ `cat /boot/config.txt | grep "dtoverlay=pi4-enable-bt"` == "dtoverlay=pi4-enable-bt" ]]; then 
-	  echo "Bluetooth hardwarelock on.. Please reboot $HOSTNAME to apply changes"
-	  sed -i s'/dtoverlay=pi4-enable-bt/#dtoverlay=pi4-enable-bt/'g /boot/config.txt 
-	  cat /boot/config.txt | grep 'dtoverlay=pi4-enable-bt' --color=none
-  	else
-	  echo "Bluetooth harware lock off.. Please reboot $HOSTNAME to apply changes"
-	  sed -i s'/#dtoverlay=pi4-enable-bt/dtoverlay=pi4-enable-bt/'g /boot/config.txt
-	  cat /boot/config.txt | grep 'dtoverlay=pi4-enable-bt' --color=none
-	fi
-fi
+function check_root {
+  if [ "$(id -u)" != "0" ]; then 
+    echo "This script must be run as root" 1>&2 exit 1
+  fi
+}
+
+function enable_disable_bluetooth {
+    if [[ -z `cat /boot/config.txt | grep "dtoverlay=pi4-enable-bt"` ]]; then
+        echo "Bluetooth enabled.. Please reboot $HOSTNAME to apply changes"
+        echo -e "\n# Uncomment to enable bluetooth\ndtoverlay=pi4-enable-bt" >> /boot/config.txt
+        cat /boot/config.txt | grep 'dtoverlay=pi4-enable-bt' --color=none
+    else
+	    if [[ `cat /boot/config.txt | grep "dtoverlay=pi4-enable-bt"` == "dtoverlay=pi4-enable-bt" ]]; then 
+                echo "Bluetooth hardwarelock on.. Please reboot $HOSTNAME to apply changes"
+	        sed -i s'/dtoverlay=pi4-enable-bt/#dtoverlay=pi4-enable-bt/'g /boot/config.txt 
+	        cat /boot/config.txt | grep 'dtoverlay=pi4-enable-bt' --color=none
+  	    else
+	        echo "Bluetooth harware lock off.. Please reboot $HOSTNAME to apply changes"
+	        sed -i s'/#dtoverlay=pi4-enable-bt/dtoverlay=pi4-enable-bt/'g /boot/config.txt
+	        cat /boot/config.txt | grep 'dtoverlay=pi4-enable-bt' --color=none
+	   fi
+    fi
+}
+
+function main {
+    check_root
+    enable_disable_bluetooth
+}
+
+main
