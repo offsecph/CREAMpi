53a2013078ea0f44fabf88933f0ebe4776fde0e9
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-10-08 19:21:42 +0800

    Update check_storage.sh

--- scripts/hardware/check_storage.sh
+++ scripts/hardware/check_storage.sh
@@ -3,12 +3,23 @@
 # Checks the current storage of rasberry pi
 #
 
-function main {
+function check_root {
+  if [ "$(id -u)" != "0" ]; then 
+    echo "This script must be run as root" 1>&2 exit 1
+  fi
+}
+
+function check_storage {
   df -h
   echo
   df -h | grep /root -B1
   echo
   duf
+}
+
+function main {
+    check_root
+    check_storage
 }
 
 main
