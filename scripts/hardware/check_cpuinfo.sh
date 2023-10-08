ae5d6cb04778452fa5f50641b948dbc94c118a75
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-10-08 19:20:26 +0800

    Update check_cpuinfo.sh

--- scripts/hardware/check_cpuinfo.sh
+++ scripts/hardware/check_cpuinfo.sh
@@ -3,9 +3,20 @@
 # Checks the hardware info of the pi
 #
 
-function main {
+function check_root {
+  if [ "$(id -u)" != "0" ]; then 
+    echo "This script must be run as root" 1>&2 exit 1
+  fi
+}
+
+function check_cpuinfo {
   echo -e "------------------- CPUINFO -------------------"
   lscpu | grep 'Architecture' -A20 
+}
+
+function main {
+    check_root
+    check_cpuinfo
 }
 
 main
