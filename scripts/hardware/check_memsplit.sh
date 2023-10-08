a1c379a3958a569402d7e17cb691f5a51b86c428
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-10-08 19:21:02 +0800

    Update check_memsplit.sh

--- scripts/hardware/check_memsplit.sh
+++ scripts/hardware/check_memsplit.sh
@@ -2,12 +2,24 @@
 #
 # Checks the memory split between cpu and gpu
 #
-function main {
+
+function check_root {
+  if [ "$(id -u)" != "0" ]; then 
+    echo "This script must be run as root" 1>&2 exit 1
+  fi
+}
+
+function check_memsplit {
  echo -e "Checking current memory split of CPU and GPU:\n"
  echo "ARM64:"
  vcgencmd get_mem arm | cut -d'=' -f2
  echo -e "\nGPU:"
  vcgencmd get_mem gpu | cut -d'=' -f2
+}
+
+function main {
+    check_root
+    check_memsplit
 }
 
 main
