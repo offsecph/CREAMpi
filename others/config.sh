28fe748a195403340302b4b97cb9014f7e64fd7e
War Torrente I <42901134+catx0rr@users.noreply.github.com>
2023-10-21 22:34:04 +0800

    Update config.sh

--- others/config.sh
+++ others/config.sh
@@ -43,44 +43,11 @@ function configure_resolved() {
     if [ ! -f /etc/systemd/resolved.conf.bak ]; then
         cp -fv /etc/systemd/resolved.conf /etc/systemd/resolved.conf.bak
     fi
-    rm -rf /etc/systemd/resolved.conf
-    cat > /etc/systemd/resolved.conf << EOF
-#  This file is part of systemd.
-#
-#  systemd is free software; you can redistribute it and/or modify it under the
-#  terms of the GNU Lesser General Public License as published by the Free
-#  Software Foundation; either version 2.1 of the License, or (at your option)
-#  any later version.
-#
-# Entries in this file show the compile time defaults. Local configuration
-# should be created by either modifying this file, or by creating "drop-ins" in
-# the resolved.conf.d/ subdirectory. The latter is generally recommended.
-# Defaults can be restored by simply deleting this file and all drop-ins.
-#
-# Use 'systemd-analyze cat-config systemd/resolved.conf' to display the full config.
-#
-# See resolved.conf(5) for details.
-
-[Resolve]
-# Some examples of DNS servers which may be used for DNS= and FallbackDNS=:
-# Cloudflare: 1.1.1.1#cloudflare-dns.com 1.0.0.1#cloudflare-dns.com 2606:4700:4700::1111#cloudflare-dns.com 2606:4700:4700::1001#cloudflare-dns.com
-# Google:     8.8.8.8#dns.google 8.8.4.4#dns.google 2001:4860:4860::8888#dns.google 2001:4860:4860::8844#dns.google
-# Quad9:      9.9.9.9#dns.quad9.net 149.112.112.112#dns.quad9.net 2620:fe::fe#dns.quad9.net 2620:fe::9#dns.quad9.net
-DNS=1.1.1.1 
-FallbackDNS=9.9.9.9
-Domains=dns.cloudflare.com dns.quad9.net
-DNSSEC=yes
-DNSOverTLS=yes
-#MulticastDNS=yes
-#LLMNR=yes
-#Cache=yes
-#CacheFromLocalhost=no
-#DNSStubListener=yes
-#DNSStubListenerExtra=
-#ReadEtcHosts=yes
-#ResolveUnicastSingleLabel=no
-#StaleRetentionSec=0
-EOF
+    sed -r -i "s/#DNS=/DNS=1.1.1.1/" /etc/systemd/resolved.conf
+    sed -r -i "s/#FallbackDNS=/FallbackDNS=9.9.9.9/" /etc/systemd/resolved.conf
+    sed -r -i "s/#Domains=/Domains=cloudflare-dns.com dns.quad9.net/" /etc/systemd/resolved.conf
+    sed -r -i "s/#DNSSEC=no/DNSSEC=yes/" /etc/systemd/resolved.conf
+    sed -r -i "s/#DNSOverTLS=no/DNSOverTLS=yes/" /etc/systemd/resolved.conf
 }
 
 function configure_networkmanager() {
@@ -123,7 +90,7 @@ function configure_iptables() {
 }
 
 function enable_services() {
-    systemctl enable systemd-resolved && sleep 2
+    systemctl enable systemd-resolved && sleep 5
     systemctl restart systemd-resolved
     systemctl enable --now iptables-persistent.service
     systemctl restart NetworkManager
